8edf517572c5d93280c4b21770370ce10266e24b
Marcio <marciofujikura@hotmail.com>
2020-02-26 23:11:44 -0300

    Ajustando o Adapter

--- app/src/main/java/br/com/alura/agenda/asycntask/BuscaAlunoTask.java
+++ app/src/main/java/br/com/alura/agenda/asycntask/BuscaAlunoTask.java
@@ -2,10 +2,13 @@ package br.com.alura.agenda.asycntask;
 
 import android.os.AsyncTask;
 
+import java.util.List;
+
 import br.com.alura.agenda.database.dao.AlunoDAO;
+import br.com.alura.agenda.model.Aluno;
 import br.com.alura.agenda.ui.adapter.ListaAlunosAdapter;
 
-public class BuscaAlunoTask extends AsyncTask {
+public class BuscaAlunoTask extends AsyncTask<Void, Void, List<Aluno>> {
 
     private final ListaAlunosAdapter adapter;
     private final AlunoDAO dao;
@@ -16,8 +19,13 @@ public class BuscaAlunoTask extends AsyncTask {
     }
 
     @Override
-    protected Object doInBackground(Object[] objects) {
-        adapter.atualiza(dao.todos());
-        return null;
+    protected List<Aluno> doInBackground(Void... voids) {
+        return dao.todos();
+    }
+
+    @Override
+    protected void onPostExecute(List<Aluno> alunos) {
+        super.onPostExecute(alunos);
+        adapter.atualiza(alunos);
     }
 }
